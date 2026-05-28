afe9cf8a8505670aefc521578d334e271905afc1
Xander Bredenkamp <xrbmenlyn65@gmail.com>
2026-05-28 18:55:07 +0200

    SectionA Question3, Final 1.0

--- SDT621_FA1_XanderRB_20240087/SectionA_Question3/Form1.Designer.cs
+++ SDT621_FA1_XanderRB_20240087/SectionA_Question3/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtLanguageInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstLanguages = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLanguageInput
+            // 
+            this.txtLanguageInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLanguageInput.Location = new System.Drawing.Point(12, 44);
+            this.txtLanguageInput.Name = "txtLanguageInput";
+            this.txtLanguageInput.Size = new System.Drawing.Size(259, 30);
+            this.txtLanguageInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 80);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(107, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Langiage";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstLanguages
+            // 
+            this.lstLanguages.FormattingEnabled = true;
+            this.lstLanguages.ItemHeight = 16;
+            this.lstLanguages.Location = new System.Drawing.Point(12, 121);
+            this.lstLanguages.Name = "lstLanguages";
+            this.lstLanguages.Size = new System.Drawing.Size(171, 132);
+            this.lstLanguages.TabIndex = 2;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 274);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(143, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove Selected";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(12, 391);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(252, 41);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "System Ready";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input desired language";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lstLanguages);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtLanguageInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLanguageInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstLanguages;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SDT621_FA1_XanderRB_20240087/SectionA_Question3/Form1.cs
+++ SDT621_FA1_XanderRB_20240087/SectionA_Question3/Form1.cs
@@ -16,5 +16,79 @@ namespace SectionA_Question3
         {
             InitializeComponent();
         }
+
+        //Event handler for the Add button
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            //Cleanup the user input by trimming accidental whitespace
+            string languageInput = txtLanguageInput.Text.Trim();
+
+            //Validation: Prevent empty list
+            if (string.IsNullOrEmpty(languageInput))
+            {
+                lblStatus.Text = "Error: Please enter a programming language name.";
+                return;
+            }
+
+            //Validation: Prevent dupelicates
+            bool isDuplicate = false;
+            foreach (var item in lstLanguages.Items)
+            {
+                if (item.ToString().Equals(languageInput, StringComparison.OrdinalIgnoreCase))
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (isDuplicate)
+            {
+                lblStatus.Text = $"Error: '{languageInput}' is already in the list.";
+                txtLanguageInput.Clear();
+                txtLanguageInput.Focus();
+                return;
+            }
+
+            //Action: Add to list and track timestop
+            lstLanguages.Items.Add(languageInput);
+
+            //Update status display with current date and time
+            lblStatus.Text = $"Added '{languageInput}' at {DateTime.Now.ToString("HH:mm:ss")}";
+
+            //Reset input box for user convenience
+            txtLanguageInput.Clear();
+            txtLanguageInput.Focus();
+        }
+
+        //Event handler for the remove button
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //Check if the user clicked on an item in the list box
+            if (lstLanguages.SelectedIndex != -1)
+            {
+                string removedLanguage = lstLanguages.SelectedItem.ToString();
+
+                //remove the selected item
+                lstLanguages.Items.RemoveAt(lstLanguages.SelectedIndex);
+
+                //Update status with action timestamp
+                lblStatus.Text = $"Removed '{removedLanguage}' at {DateTime.Now.ToString("HH:mm:ss")}";
+            }
+            else
+            {
+                lblStatus.Text = "Error: Select a language from the list to remove.";
+            }
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
